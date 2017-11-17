347c6bbc0d7d641ccc1ca7840523e5e677f212ff
Lachlan Deer <lachlan.deer@gmail.com>
2017-11-17 17:10:13 +0100

    remove function that only computes vadercounts and not the aggregate statistics

--- lib/computeVaderResults.py
+++ lib/computeVaderResults.py
@@ -262,46 +262,6 @@ def vaderCountsByClassification(dataset, identifier):
                         , ascending=False)
     return vaderCounts
 
-def computeVaderCounts(dataset, movieList):
-    """
-    Calcuates the number of positive, negative and neutral
-    tweets for an individual movie per day, from a list of
-    movies.
-
-    Inputs:
-        - dataset: a spark DataFrame with tweets
-            classified into buckets
-        - movieList: a list of movies to compute stats for
-    Other Functions Called:
-        - singleMovieTweets()
-        - vaderCountsByClassification()
-    Outputs:
-        - indivCounts: a spark DataFrame with each
-            movie-days number of tweets per classification
-    Example Usage:
-        computeVaderCounts(classified_data, list_of_movies)
-    """
-    # for all unique movies
-    for idx, movieName in enumerate(movieList):
-        # filter relevant tweets
-        indivTweets = singleMovieTweets(dataset, movieList[idx])
-        # count tweets by type
-        indivCounts = vaderCountsByClassification(indivTweets, movieList[idx])
-        # write results to a dataset, creating it if necessary
-        if 'allVaderCounts' not in locals() or 'allVaderCounts' in globals():
-            allVaderCounts = indivCounts
-        if 'allVaderCounts' in locals() or 'allVaderCounts' in globals():
-            allVaderCounts = allVaderCounts.union(indivCounts)
-
-        print(movieName,
-                'passed Lazy Evaluation at the tweet count stage')
-    # save data to a pandas data frame
-    print ('Converting Count Data to Pandas DF,
-                this may take a while...')
-    pandasVaderCounts = allVaderCounts.toPandas()
-    print('complete!')
-    return pandasVaderCounts
-
 def vaderStats(dataset, identifier, vaderCol = 'vaderScore'):
     """
     Compute Daily Sentiment Statistics for a movie
@@ -381,7 +341,7 @@ def computeMovieStats(dataset, movieList):
             allVaderStats = indivStats
         if 'allVaderStats' in locals() or 'allVaderStats' in globals():
             allVaderStats = allVaderStats.union(indivStats)
-    return allVaderCounts, allVaderStats 
+    return allVaderCounts, allVaderStats
 
 # --- CSV Writers --- #
 
