5f6d12d80d003cd1adcc6be8294ee37e8ead0a69
Lachlan Deer <lachlan.deer@gmail.com>
2017-11-21 13:57:06 +0100

    use argparse module for better imput parameter passing

--- src/main/driver_dailySentiment.py
+++ src/main/driver_dailySentiment.py
@@ -9,23 +9,64 @@ import sys
 from pathlib import Path
 import time
 import csv
+import argparse
 
 # User written
 sys.path.append(str(Path('.').absolute()))
 #import lib.computeVaderResults as cvr
 
-# --- Import CL arguments:
-dataPath    = sys.argv[1]
-analysisSet = sys.argv[2]
-thresholds  = sys.argv[3]
-outCounts   = sys.argv[4]
-outStats    = sys.argv[5]
+# defined command line options
+# this also generates --help and error handling
+CLI=argparse.ArgumentParser()
+CLI.add_argument(
+  "--dataPath",  # name on the CLI - drop the `--` for positional/required parameters
+  nargs   = "*",  # 0 or more values expected => creates a list
+  type    = str,
+  default ='alluxio://master001:19998/',  # default if nothing is provided
+)
+CLI.add_argument(
+  "--folder",
+  nargs   = "*",
+  type    = str,  # any type/callable can be used here
+  default = [],
+)
+CLI.add_argument(
+  "--thresholds",
+  nargs   ="*",
+  type    = float,  # any type/callable can be used here
+  default = [-1.0, -0.5, 0, 0.5, 1.0],
+)
+CLI.add_argument(
+  "--outCounts",
+  nargs   = "*",
+  type    = str,  # any type/callable can be used here
+  default = ['./'],
+)
+CLI.add_argument(
+  "--outStats",
+  nargs   = "*",
+  type    = str,  # any type/callable can be used here
+  default = ['./'],
+)
 
-print(dataPath)
-print(analysisSet)
-print(thresholds)
-print(outCounts)
-print(outStats)
+# parse the command line
+args = CLI.parse_args()
+# access CLI options
+print("dataPath  : %r" % args.dataPath)
+print("folder    : %r" % args.folder)
+print("thresholds: %r" % args.thresholds)
+print("outCounts : %r" % args.outCounts)
+print("outStats  : %r" % args.outStats)
 
-file1 = csv.writer(open(outCounts, "wb"))
-file2 = csv.writer(open(outStats, "wb"))
+
+# rowText = [1,2,3, "hat"]
+#
+# ofile  = open(outCounts, "w")
+# writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+# writer.writerow(rowText)
+# ofile.close()
+#
+# ofile  = open(outStats, "w")
+# writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+# writer.writerow(rowText)
+# ofile.close()
