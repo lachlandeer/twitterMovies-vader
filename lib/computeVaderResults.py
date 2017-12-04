4c0777403927282e14b050344bf3deb9e0a4792d
Lachlan Deer <lachlan.deer@gmail.com>
2017-12-04 12:02:43 +0100

    fix bad reference to data set

--- lib/computeVaderResults.py
+++ lib/computeVaderResults.py
@@ -487,7 +487,7 @@ def parseMovieData(filePath, outStats, outCounts, textCol = 'body',
 
     for iMovie in moviesUnique:
         # recover counts and summary stats
-        vaderCounts, vaderStats = computeMovieStats2(classifiedData, iMovie)
+        vaderCounts, vaderStats = computeMovieStats2(df, iMovie)
 
         # add to data set or create them if they dont exist
         # first, vader counts
