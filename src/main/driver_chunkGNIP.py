5226e155f2d9962d49fe3a5c513285db0618003f
Lachlan Deer <lachlan.deer@gmail.com>
2017-12-07 13:28:46 +0100

    start a script to process the GNIPFIlter chunking

---
+++ src/main/driver_chunkGNIP.py
@@ -0,0 +1,60 @@
+"""
+Here is some basic text
+"""
+# --- Import Libraries --- #
+## Native Python
+import os
+import sys
+from pathlib import Path
+import time
+import csv
+import argparse
+
+## User written
+#sys.path.append(str(Path('.').absolute()))
+import computeVaderResults as cvr
+
+# ---  Define command line options --- #
+# this also generates --help and error handling
+CLI=argparse.ArgumentParser()
+CLI.add_argument(
+  "--dataPath",  # name on the CLI - drop the `--` for positional/required parameters
+  nargs   = "*",  # 0 or more values expected => creates a list
+  type    = str,
+  default ='alluxio://master001:19998/',  # default if nothing is provided
+)
+CLI.add_argument(
+  "--folder",
+  nargs   = "*",
+  type    = str,  # any type/callable can be used here
+  default = [],
+)
+CLI.add_argument(
+  "--outListFolder",
+  nargs   = "*",
+  type    = str,  # any type/callable can be used here
+  default = ['./'],
+)
+
+
+# --- Parse the Command Line Options --- #
+args = CLI.parse_args()
+print('Running PySpark in batch mode...')
+print("-------------------------------------------")
+print("Here are the specs for this job:")
+# access CLI options
+print("dataPath      : %r" % args.dataPath)
+print("folder        : %r" % args.folder)
+print("Lists saved to: %r" % args.outListFolder)
+print("-------------------------------------------")
+
+dataPath        = args.dataPath[0]
+folderPath      = args.folder[0]
+outListFolder   = args.outListFolder[0]
+
+# --- Run analysis --- #
+startTime = time.time()
+print('Starting Job')
+
+
+cvr.parseMovieData(
