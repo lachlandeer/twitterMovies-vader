946fd141259e4c00b8312b877b07dd4bb713e7e6
lachlan <lachlan.deer@uzh.ch>
2017-12-11 01:02:29 +0100

    destring None in movie list checker

--- lib/computeVaderResults.py
+++ lib/computeVaderResults.py
@@ -437,7 +437,7 @@ def processGNIPFilters(dataPath, outPath):
 # --- Run VADER analysis ---#
 
 def parseMovieData(dataPath, outStats, outCounts,
-                        movieList = 'None',textCol = 'body',
+                        movieList = None,textCol = 'body',
                         thresholds = [-1.0, -0.5, 0.5, 1.0]):
 
     # Load Data
@@ -449,7 +449,9 @@ def parseMovieData(dataPath, outStats, outCounts,
     df = vaderClassify(df, vScore = 'vaderScore',
                         outCol = 'vaderClassifier', thresholds=thresholds)
 
-    if movieList == 'None':
+    print('The value of movie List is: ', movieList)
+
+    if movieList == None:
         print('No movieList passed across, finding unique movies in data')
         movies = uniqueMovies(df, 'movieName')
         print(len(movies), ' movies in ', dataPath)
